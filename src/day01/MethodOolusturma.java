0e7d0f0e5bba05dec7d4d9554ff2dfcea5d696f4
mhmmdylmz07 <mylmz3219@gmail.com>
2021-04-10 13:40:30 +0300

    繰り返すこと

---
+++ src/day01/MethodOolusturma.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class MethodOolusturma {
+    public static void main(String[] args) {
+        こんにちは();
+    }
+
+    private static void こんにちは() {
+        System.out.println("こんにちは" );
+    }
+}
