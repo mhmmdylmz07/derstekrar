a23c9a8afc85543ce076c13d660b750be7f4e202
mhmmdylmz07 <mylmz3219@gmail.com>
2021-04-10 14:10:07 +0300

    ikinci

--- src/day01/MethodOolusturma.java
+++ src/day01/MethodOolusturma.java
@@ -2,7 +2,8 @@ package day01;
 
 public class MethodOolusturma {
     public static void main(String[] args) {
-        System.out.println("こんにちは" );
+        System.out.println("mer");
+        System.out.println("mehaba");
         こんにちは();
     }
 
